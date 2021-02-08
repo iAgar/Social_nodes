b2c8323582f56ea2289c4f00f5562a2f46fa36c4
Ishaan Agarwal <ishaanagarwal1805@gmail.com>
2021-02-08 20:15:33 +0530

    Set up sign up feature by creating a document in mongodb

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,3 +1,5 @@
+const User = require('../models/user_schema');
+
 module.exports.signUp = function(req, res){
     return res.render('sign-up', {
         title: 'Sign Up'
@@ -10,3 +12,19 @@ module.exports.signIn = function(req, res){
     });
 }
 
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confPassword)
+    {
+        console.log("The passwords entered did not match");
+        return res.redirect('back');
+    }
+
+    User.create(req.body, function(err, User){
+        if(err){
+            console.log('Error'. err);
+            return res.redirect('back');
+        }
+
+        return res.redirect('/user/sign-in');
+    });
+}
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userControllers = require('../controllers/user_controllers');
 
 router.get('/sign-up', userControllers.signUp);
 router.get('/sign-in', userControllers.signIn);
+router.post('/create', userControllers.create);
 
 module.exports = router
