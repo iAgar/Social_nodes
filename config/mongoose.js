266b2d0e4a33fc5a4c45d358a9f697fea273e626
Ishaan Agarwal <70597343+iAgar@users.noreply.github.com>
2021-04-26 14:29:14 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
-mongoose.connect('mongodb://localhost/social_nodes_production'||env.db);
+mongoose.connect(env.db);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
